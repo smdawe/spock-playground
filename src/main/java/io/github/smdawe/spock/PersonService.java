14c2cb8e916e4a872349b170ed22dbf26427763a
Simon Dawe <simon.dawe@oodlefinance.com>
2022-06-07 14:47:10 +0100

    Update PersonService.java

--- src/main/java/io/github/smdawe/spock/PersonService.java
+++ src/main/java/io/github/smdawe/spock/PersonService.java
@@ -2,7 +2,7 @@ package io.github.smdawe.spock;
 
 public class PersonService {
 
-  // valid ids ust start with P followed by 4 digits
+  // valid ids must start with P followed by 4 digits
   private static boolean validateId(String id) {
     return true;
   }
